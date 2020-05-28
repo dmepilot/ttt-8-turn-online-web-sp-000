23ca6b612678ec2a169181d2e4deee879271d823
Dean Erickson <dmepilot@yahoo.com>
2020-05-28 04:25:24 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  input_to_index(input)
+  if valid_move?
+    move(board, index, char)
+  else turn(board)
+  end
+end
+  
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  return TRUE if index.between?(0,8) && (board[index] != ("X" || "O") )
+end
+
+def input_to_index(input)
+  index=input.to_i
+  index -= 1
+end
+
+def move(board, index, char="X")
+  board[index] = char
+end
+
